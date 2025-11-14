32269774f225966b70d6026307684570b9ce48f4
DaniaGarmon <dania.gmontiel@gamil.com>
2025-11-14 13:39:42 -0600

    Generación de curp dado a los datos de usuario

--- generador_curp.py
+++ generador_curp.py
@@ -5,19 +5,12 @@ email: aspirante02_a26@cic.ipn.mx
 """
 
 #Variables
-apellido_p = None
-appelido_m = None
-nombre = None
-fecha_nacimiento = None
-sexo = None
-lugar_nacimiento = None
-datos_usuario= None
 continuar = True
 contador = 0
 
 #Constantes
-Vocales = ['a','e','i','o','u']
-Cat_sexo = ['H','M']
+Vocales = ['A','E','I','O','U']
+Cat_sexo = ['H','M','X']
 Cat_entidades = {
     "AGUASCALIENTES":"AS",
     "BAJA CALIFORNIA":"BC",
@@ -27,8 +20,8 @@ Cat_entidades = {
     "COLIMA": "CM",
     "CHIAPAS": "CS", 
     "CHIHUAHUA": "CH",
-    "DISTRITO": "FEDERAL",
-    "DF DURANGO": "DG",
+    "CIUDAD DE MÉXICO": "DF",
+    "DURANGO": "DG",
     "GUANAJUATO": "GT",
     "GUERRERO": "GR",
     "HIDALGO":"HG",
@@ -53,6 +46,20 @@ Cat_entidades = {
     "ZACATECAS":"ZS",
     "NACIDO EN EL EXTRANJERO":"NE"
 }
+
+def seleccionar_estado_nacimiento():
+  global Cat_entidades
+  print('Cátalogo de entidades:')
+  for estado, clave in Cat_entidades.items():
+    print(f'{estado} - {clave}')
+  print('---------------------------------')
+  estado = input('Ingresa el código del estado donde naciste. Por ejemplo, si naciste en Tlaxcala, ingresa "TL": ').upper()
+  if estado in Cat_entidades.values():
+    return estado
+  else:
+    print('Estado no válido, intenta de nuevo.')
+    print('---------------------------------')
+    return seleccionar_estado_nacimiento()
 
 def verificar_dia_nacimiento(dia, mes, anio):
   g_30 = [4,6,9,11]
@@ -97,7 +104,9 @@ def verificar_datos(dato,caso, mes=None, anio=None):
     case 1:
       if len(dato) >=3:
         contador = 0
-        return dato.upper()
+        dato = dato.upper()
+        dato = [d for d in dato]
+        return dato
       else:
         contador += 1
         if contador == 1 or contador == 2:
@@ -114,7 +123,9 @@ def verificar_datos(dato,caso, mes=None, anio=None):
     case 2:
         if len(dato) >=3:
           contador = 0
-          return dato.upper()
+          dato = dato.upper()
+          dato = [d for d in dato]
+          return dato
         else:
           contador += 1
           if contador == 1 or contador == 2:
@@ -131,7 +142,9 @@ def verificar_datos(dato,caso, mes=None, anio=None):
     case 3:
         if len(dato) >=3 or dato == 'x' or dato == 'X':
             contador = 0
-            return dato.upper()
+            dato = dato.upper()
+            dato = [d for d in dato]
+            return dato
         else:
             contador += 1
             if contador == 1 or contador == 2:
@@ -169,7 +182,7 @@ def verificar_datos(dato,caso, mes=None, anio=None):
           return dato
         elif anio == 2025 and (dato ==11 or dato ==12):
           print('El sistema solo admite fechas hasta el 31 de Octubre de 2025')
-          return dato
+          return None
         else:
           return dato
       else:
@@ -183,40 +196,129 @@ def verificar_datos(dato,caso, mes=None, anio=None):
         dato = int(input('Día no válido, intenta de nuevo:'))
         return verificar_datos(dato,6,mes,anio)
 
-
-        
-
 def ingresar_fecha():
   fecha_nacimiento = []
   anio = int(input('Ingresa el año de tu nacimiento: '))
   anio = verificar_datos(anio,4)
   mes = int(input('Ingresa el mes de tu nacimiento en número, ejemplo: \nSi naciste en el mes de Noviembre ingresa el número 11: '))
   mes = verificar_datos(mes,5,None,anio)
+  if mes is None:
+    return None
   dia = int(input('Ingresa el día de tu nacimiento: '))
   dia = verificar_datos(dia,6,mes,anio)
-  fecha_nacimiento.append(anio)
+  anio_digitos = [int(d) for d in str(anio)]
+  anio_digitos = anio_digitos[2:5]
+  anio_digitos = ''.join(map(str,anio_digitos))
+  mes = str(mes).zfill(2)
+  dia = str(dia).zfill(2)
+  fecha_nacimiento.append(anio_digitos)
   fecha_nacimiento.append(mes)
   fecha_nacimiento.append(dia)
   return fecha_nacimiento
-        
+
+def seleccionar_sexo():
+  global Cat_sexo
+  sexo = int(input('Selecciona tu sexo: \nSi eres Mujer ingresa 1 \nSi eres Hombre ingresa 2 \nSi eres No Binario ingresa 3: '))
+  if sexo == 1:
+    return Cat_sexo[1]
+  elif sexo == 2:
+    return Cat_sexo[0]
+  elif sexo == 3:
+    return Cat_sexo[2]
+  else:
+    print('Opción no válida, intenta de nuevo.')
+    return seleccionar_sexo()
+      
 def ingresar_datos():
-    datos_usuario = []
     nombre = input('Ingresa tu(s) nombre(s):')
     nombre = verificar_datos(nombre,1)
-    if nombre != None:
-      datos_usuario.append(nombre) 
-    else: 
+    if nombre is None:
       return None
     apellido_p = input('Ingresa tu primer apellido:')
     apellido_p = verificar_datos(apellido_p,2)
-    datos_usuario.append(apellido_p)
+    if apellido_p is None:
+      return None
     appelido_m = input('Ingresa tu segundo apellido, en caso de no contar con este ingresa x:')
     appelido_m = verificar_datos(appelido_m,3)
-    datos_usuario.append(appelido_m)
+    if appelido_m is None:
+      return None
     fecha_nacimiento = ingresar_fecha()
-    datos_usuario = [nombre,apellido_p,appelido_m, fecha_nacimiento]
+    if fecha_nacimiento is None:
+      return None
+    sexo = seleccionar_sexo()
+    edo_nacimiento = seleccionar_estado_nacimiento()
+    datos_usuario = {
+      'nombre':nombre,
+      'apellido_p':apellido_p,
+      'apellido_m':appelido_m,
+      'fecha_nacimiento':fecha_nacimiento,
+      'sexo':sexo,
+      'edo_nacimiento':edo_nacimiento
+    }
     return datos_usuario
-    
+  
+def seleccionar_letras_apellido_p(apellido_p):
+  global Vocales
+  primer_letra = apellido_p[0]
+  apellido_p = apellido_p[1:]
+  primera_vocal = None
+  for letra in apellido_p:
+    if letra in Vocales:
+      primera_vocal = letra
+      break
+  primera_consonante = None
+  for letra in apellido_p:
+    if letra not in Vocales:
+      primera_consonante = letra
+      break
+  letras_apellido_p = [primer_letra,primera_vocal,primera_consonante]
+  return letras_apellido_p
+
+def seleccionar_letras_apellido_m(apellido_m):
+  global Vocales
+  if 'X' in apellido_m:
+    return ['X','X']
+  primera_letra = apellido_m[0]
+  apellido_m = apellido_m[1:]
+  primera_consonante = None
+  for letra in apellido_m:
+    if letra not in Vocales:
+      primera_consonante = letra
+      break
+  return [primera_letra,primera_consonante]
+
+def checar_nombres_compuestos(nombre):
+  nombres_compuestos_validar = ['MARÍA','MARIA','JOSE','JOSÉ']
+  if ' ' in nombre:
+    indice= nombre.index(' ')
+    nombre_compuesto = nombre[:indice]
+    nombre_compuesto = ''.join(nombre_compuesto)
+    if nombre_compuesto in nombres_compuestos_validar:
+      return nombre[indice+1:]
+    else:
+      return nombre
+  else:
+    return nombre
+
+def seleccionar_letras_nombre(nombre):
+  global Vocales
+  nombre = checar_nombres_compuestos(nombre)
+  primera_letra = nombre[0]
+  nombre = nombre[1:]
+  consonante_interna = None
+  for letra in nombre:
+    if letra not in Vocales:
+      consonante_interna = letra
+      break
+  return [primera_letra,consonante_interna]
+
+def verificar_palabra_altisonante(palabra):
+  palabras_reservadas = ['VACA','COLA','PENE','PUTO','MULA','JOTO','CACA','RATA']
+  if palabra in palabras_reservadas:
+    return True
+  else:
+    return False
+ 
 def generar_curp():
   print('¡Bienvenid@ al generador de CURP!')
   while continuar:
@@ -224,7 +326,34 @@ def generar_curp():
     if datos_usuario is None:
       print('Nos vemos a la próxima!!')
       break
-    print(datos_usuario)
+    letras_apellido_p= seleccionar_letras_apellido_p(datos_usuario['apellido_p'])
+    letras_apellido_m = seleccionar_letras_apellido_m(datos_usuario['apellido_m'])
+    letras_nombre = seleccionar_letras_nombre(datos_usuario['nombre'])
+    curp = [letras_apellido_p[0],
+            letras_apellido_p[1],
+            letras_apellido_m[0],
+            letras_nombre[0],
+            datos_usuario['fecha_nacimiento'][0],
+            datos_usuario['fecha_nacimiento'][1],
+            datos_usuario['fecha_nacimiento'][2],
+            datos_usuario['sexo'],
+            datos_usuario['edo_nacimiento'],
+            letras_apellido_p[2],
+            letras_apellido_m[1],
+            letras_nombre[1],
+            'XX']
+    palabra_verificar = curp[0:4]
+    palabra_verificar = ''.join(palabra_verificar)
+    palabra_altisonante = verificar_palabra_altisonante(palabra_verificar)
+    if palabra_altisonante:
+      curp[1] = 'X'
+    curp = ''.join(curp)
+    print('\n')
+    print(f'Clave Única de Registro de Población (CURP): {curp}')
     break
 
-generar_curp()
+def main():
+    generar_curp()
+
+if __name__ == '__main__':
+    main()
